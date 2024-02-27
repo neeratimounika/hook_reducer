8075c2a43356a26b2e599f42ba3abe69d4763586
mounika.neerati <mounika.neerati93@gmail.com>
2024-02-27 17:01:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CounterOne from './components/CounterOne'
+
+
+const App = () => {
+  return (
+    <div>
+      <CounterOne/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CounterOne.js
@@ -0,0 +1,32 @@
+import React, { useReducer } from 'react'
+
+const initialState=0
+
+const reducer=(state,action)=>{
+    switch(action){
+        case 'increment':
+            return state+1 
+        case 'decrement':
+            return state-1 
+        case 'reset':
+            return initialState 
+        default:
+            return state
+    }
+
+}
+
+const CounterOne = () => {
+   const [count,dispatch] =useReducer(reducer,initialState)
+
+  return (
+    <center>
+        <div>Count -{count}</div>
+      <button onClick={()=>dispatch('increment')}>Increment</button>
+      <button onClick={()=>dispatch('decrement')}>Decrement</button>
+      <button onClick={()=>dispatch('reset')}>Reset</button>
+    </center>
+  )
+}
+
+export default CounterOne
